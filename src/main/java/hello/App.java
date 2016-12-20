ee1ddbccca2cbd9d3eaa2b4e78e89d941e99a69d
Jessie Dempster <jessie.dempster@contino.io>
2016-12-21 00:00:20 +0800

    WIP.

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.PathSelectors;
@@ -18,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @ComponentScan("hello")
-public class App {
+public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
